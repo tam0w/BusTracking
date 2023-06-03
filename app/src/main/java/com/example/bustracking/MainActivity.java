8c88a541fcb2f735e05e3dc130b780a561739087
Tamim <tammu1000@gmail.com>
2023-06-03 07:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bustracking/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bustracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button track = findViewById(R.id.btn1);
+        Button login = findViewById(R.id.login);
+        Button routeList = findViewById(R.id.routelist);
+        Button drivInfo = findViewById(R.id.driverinfo);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login_type.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
