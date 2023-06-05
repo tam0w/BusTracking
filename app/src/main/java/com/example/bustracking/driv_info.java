cc8eadca84afa0542c65edb82edf9fa821a3d88b
Tamim <tammu1000@gmail.com>
2023-06-05 08:31:45 +0530

    v1

--- app/src/main/java/com/example/bustracking/driv_info.java
+++ app/src/main/java/com/example/bustracking/driv_info.java
@@ -3,6 +3,13 @@ package com.example.bustracking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.ref.Reference;
 
 public class driv_info extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class driv_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driv_info);
+        Button btn = findViewById(R.id.stop);
+        TextView name = findViewById(R.id.name);
+        TextView phno = findViewById(R.id.phno);
+        TextView route = findViewById(R.id.route);
+        TextView id = findViewById(R.id.idinfo);
+        EditText text = findViewById(R.id.id2);
+        String vxtalue = text.getText().toString();
+
+        Reference ref = FirebaseDatabase.getInstance().getReference().child('test');
     }
 }
