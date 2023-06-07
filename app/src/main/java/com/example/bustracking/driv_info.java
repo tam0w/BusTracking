ebbf45b92d5b3b6e5e7aca290e94c3de6231660c
Tamim <tammu1000@gmail.com>
2023-06-07 12:16:38 +0530

    firebase connection

--- app/src/main/java/com/example/bustracking/driv_info.java
+++ app/src/main/java/com/example/bustracking/driv_info.java
@@ -1,17 +1,26 @@
 package com.example.bustracking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.ref.Reference;
 
 public class driv_info extends AppCompatActivity {
+
+    private DatabaseReference dataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +32,32 @@ public class driv_info extends AppCompatActivity {
         TextView route = findViewById(R.id.route);
         TextView id = findViewById(R.id.idinfo);
         EditText text = findViewById(R.id.id2);
-        String vxtalue = text.getText().toString();
 
-//        Reference ref = FirebaseDatabase.getInstance().getReference().child('test');
+        // Create a database reference to the desired node
+        dataRef = FirebaseDatabase.getInstance().getReference();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String node = text.getText().toString().trim();
+                retrieveDataFromNode(node, name);
+            }
+        });
+    }
+
+    private void retrieveDataFromNode(String node, final TextView textView) {
+        dataRef.child(node).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Retrieve and process the data
+                String data = dataSnapshot.getValue(String.class);
+                textView.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle any errors that occur during data retrieval
+            }
+        });
     }
 }
